847409700e9858bf9d24f45e90b161da0e42af15
ChrisSimoneau <100176959+ChrisSimoneau@users.noreply.github.com>
2022-10-14 14:25:52 -0300

    Add files via upload

---
+++ cipher.py
@@ -0,0 +1,50 @@
+## This is an augmented version of the Caesar cipher, where the cipher shifts values after every letter making it harder
+
+
+## ask user for a message to be encrypted
+message = input("Enter a message to be encrypted: ")
+
+## holds the encrypted message
+encrypted = ""
+
+## create a variable to hold the shift amount
+shift = 0
+
+## loop through each letter in the message (ignoring spaces and numbers) to be encrypted
+for letter in message:
+    if letter.isalpha():
+        ## check the unicode to determine what and if it its a letter
+        value = ord(letter)
+        ## add the shift amount to the unicode number
+        value += shift
+        ## Check if its uppercase
+        if letter.isupper():
+            ## if the value is greater than Z to reset to A
+            if value > ord("Z"):
+                value -= 26
+            elif value < ord("A"):
+                ## add 26 to the value
+                value += 26
+        ## if the letter is lowercase
+        elif letter.islower():
+            ## if the value is greater than z subtract 26
+            if value > ord("z"):
+                value -= 26
+                
+            ## if the value is less than a
+            elif value < ord("a"):
+                value += 26
+
+        ## convert the value back to a letter
+        letter = chr(value)
+        encrypted += letter
+        shift += 1
+
+    ## if the character is not a letter, add it to the encrypted message and do not shift
+    else:
+        encrypted += letter
+
+
+## display the encrypted message
+print(encrypted)
+
